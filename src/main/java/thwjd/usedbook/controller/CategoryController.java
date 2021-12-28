8ddf5d3342aec3233bf05d1ebfc120c21faffea3
thwjd <thwjd1356@naver.com>
2021-12-28 19:13:28 +0900

    feat: add category list

--- src/main/java/thwjd/usedbook/controller/CategoryController.java
+++ src/main/java/thwjd/usedbook/controller/CategoryController.java
@@ -1,22 +1,36 @@
 package thwjd.usedbook.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import thwjd.usedbook.annotation.Login;
+import thwjd.usedbook.entity.BookCategory;
+import thwjd.usedbook.entity.BookPost;
 import thwjd.usedbook.entity.Member;
+import thwjd.usedbook.repository.BookPostRepositoryMapper;
+
+import java.util.List;
 
 @Slf4j
 @Controller
 @RequestMapping("/category")
 public class CategoryController {
 
-    @GetMapping("/{param}")
-    public String category(@PathVariable String param){
+    @Autowired BookPostRepositoryMapper bookPostMapper;
 
-        return "category/"+param;
+    @GetMapping("/{param}")
+    public String category(@PathVariable String param, Model model){
+
+        List<BookPost> categoryAll = bookPostMapper.findByCategory(param);
+        model.addAttribute("lists", categoryAll);
+        model.addAttribute("categoryName", param);
+        String categoryNameKor = BookCategory.valueOf(param.toUpperCase()).getName();
+        model.addAttribute("categoryNameKor", categoryNameKor);
+
+        return "bookPost/list";
     }
 }
--- src/main/java/thwjd/usedbook/repository/BookPostRepositoryMapper.java
+++ src/main/java/thwjd/usedbook/repository/BookPostRepositoryMapper.java
@@ -8,17 +8,24 @@ import org.springframework.stereotype.Repository;
 import thwjd.usedbook.entity.BookPost;
 import thwjd.usedbook.entity.Member;
 
+import java.awt.print.Book;
 import java.util.List;
 
 @Mapper
 @Repository
 public interface BookPostRepositoryMapper {
 
-    @Insert("insert into bookpost(writeremail, bookname, bookcategory, bookprice, bookdescription) " +
-                        "values(#{writerEmail}, #{bookName}, #{bookCategory}, #{bookPrice}, #{bookDescription})")
+    @Insert("insert into bookpost(writeremail, bookname, bookcategory, bookprice, bookdescription, createtime) " +
+                        "values(#{writerEmail}, #{bookName}, #{bookCategory}, #{bookPrice}, #{bookDescription}, #{createTime})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
     void save(BookPost bookPost);
 
     @Select("select * from bookpost")
     List<BookPost> findAll();
+
+    @Select("select * from bookpost where bookcategory=#{category}")
+    List<BookPost> findByCategory(String category);
+
+    @Select("select * from bookpost where id=#{id}")
+    BookPost findById(Long id);
 }
