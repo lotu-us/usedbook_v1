c8359fcfadf5ecbd7fd2aaec032860e89c6cc809
thwjd <thwjd1356@naver.com>
2021-12-28 19:09:43 +0900

    refactor bookPost_write(newBookPost)

--- src/main/resources/static/js/bookPost_write.js
+++ src/main/resources/static/js/bookPost_write.js
@@ -11,77 +11,7 @@ inputs.on("click", function(){
     }
 });
 
-$(".previewslider .add-slideimg").on("click", function(){
-    //console.log("add-slideimg click");
-    if(uploadFileMap.size >= imgCountMax){
-        alert(imgCountMaxMessage);
-        return;
-    }
-    $("#fileUploadBtn").click();
-});
 
-
-var uploadFileMap = new Map();
-
-$("#fileUploadBtn").on("change", function(event){
-    //console.log("fileUploadBtn onChange");
-    //파일 선택 후 실행됨
-
-    var files = event.target.files; // = $("#fileUploadBtn")[0].files
-    /* FileList반환
-       [0]: File / length: 1
-       name: "캡처.JPG"(파일명), size: 107939(파일사이즈), type: "image/jpeg" (파일형식)
-       업로드 후 files는 초기화된다
-     */
-
-    var filesArr = Array.prototype.slice.call(files);
-    // File로 분리
-
-    var blobsrc = "";
-    var count = 0;
-    filesArr.forEach(function(file){
-        count++;
-        if(count > imgCountMax){
-        }
-
-        if(count <= imgCountMax){
-            if(!file.type.match("image.*")){
-                alert("이미지 확장자만 가능합니다");
-                return;
-            }
-
-            blobsrc = URL.createObjectURL(file);
-            //Blob 반환. Binary Large Object
-            //blob객체의 url주소값으로 이미지를 불러올 수 있게된다.
-            //이렇게 생성된 주소는 브라우저의 메모리에 올라가있다.
-
-            uploadFileMap.set(blobsrc, file);
-            addSlideDesign(blobsrc);
-        }
-    });
-
-    if(count > imgCountMax){
-        alert(imgCountMaxMessage+"\n10개 이후의 이미지는 삭제되었습니다.");
-    }
-
-    blobsrc.onload = function(){
-        URL.rejectObjectUrl(blobsrc);
-        //이미지 로딩 후 URL 메모리에서 해제
-    }
-//    console.log(uploadFileMap);
-//    uploadFileMap.forEach(function(v, k){
-//        console.log(k, v);
-//    });
-})
-
-
-function fileDelete(blobFileSrc){
-    uploadFileMap.delete(blobFileSrc);
-//    console.log(uploadFileMap);
-//    uploadFileMap.forEach(function(v, k){
-//        console.log(k, v);
-//    });
-}
 
 
 var validCheck = 0;
